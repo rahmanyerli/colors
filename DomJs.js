789fa4bb4581a7e38cd5399943ffbb19a12df6df
Rahman Yerli <rahmanyerli@gmail.com>
2019-01-23 09:18:39 +1100

    first commit

---
+++ DomJs.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Dialog Modals
+    // var modalTriggers = document.querySelectorAll("button[data-role=modal-show]");
+    // if (modalTriggers) {
+    //     for (const modalTrigger of modalTriggers) {
+    //         modalTrigger.addEventListener("click", showModal);
+    //     }
+    // }
+    // var modalDismissers = document.querySelectorAll("[data-role=modal-dismiss], dialog");
+    // if (modalDismissers) {
+    //     for (const modalDismisser of modalDismissers) {
+    //         modalDismisser.addEventListener("click", dismissModal);
+    //     }
+    // }
+    // Dropdowns
+    // var dropdowns = document.querySelectorAll(".dropdown");
+    // if (dropdowns) {
+    //     for (const dropdown of dropdowns) {
+    //         var button = dropdown.querySelector("button");
+    //         //var button = dropdown.querySelector("[data-toggle=dropdown]");
+    //         button.addEventListener("click", showDropdown);
+    //         button.addEventListener("blur", hideDropdown);
+    //     }
+    // }
+    // Tooltips & Popovers
+    // var tooltips = document.querySelectorAll("[title]");
+    // if (tooltips) {
+    //     for (const tooltip of tooltips) {
+    //         initTooltip(tooltip);
+    //     }
+    // }
+    // Alerts
+    // var alerts = document.querySelectorAll("div.alert");
+    // if (alerts) {
+    //     for (const alertbox of alerts) {
+    //         initAlert(alertbox);
+    //     }
+    // }
+    // var alertDismissers = document.querySelectorAll("[data-role=alert-hide]");
+    // if (alertDismissers) {
+    //     for (const alertDismisser of alertDismissers) {
+    //         alertDismisser.addEventListener("click", dismissAlert);
+    //     }
+    // }
+    // Accordions
+    // var accordions = document.querySelectorAll(".accordion");
+    // if (accordions) {
+    //     for (var accordion of accordions) {
+    //         accordion.addEventListener("click", toggleAccordion);
+    //     }
+    // }
+});
